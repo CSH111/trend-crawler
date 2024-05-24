52d80914aa08c671643eb7055e718f53e8b86bcd
CSH111 <seongho6022@naver.com>
2024-05-24 19:21:49 +0900

    로컬 db 세팅

---
+++ db.js
@@ -0,0 +1,19 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+export const zzz = 1234;
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PW,
+  database: "keyword_db",
+  port: "3306",
+});
+db.connect((e) => {
+  if (e) {
+    console.log("connection FAIL!!!!!");
+    console.log("e", e);
+    return;
+  }
+  console.log("mysql connected~");
+});
